945b02e91380dc613079560e3f28a7585b24ccf4
tokyonyquisd <antonsdl@gmail.com>
2017-05-18 02:16:33 +0300

    added weird test data

--- scripts/fill_data.sh
+++ scripts/fill_data.sh
@@ -53,5 +53,19 @@ ins_post() {
 echo ">>> inserting data"
 
 ins_user 1 "anthony" "anthonybel"
+ins_user 2 "johnny" "walker18"
+ins_user 3 "bilbo" "swaggins"
+ins_user 4 "angela" "mercy"
+ins_user 5 "han" "soloyolo"
+
 ins_meme 1 "keanu"
-ins_post 1 "first\ post" 1 1 "that feeling when..."
+ins_meme 2 "social awkward penguin"
+ins_meme 3 "man of a culture"
+ins_meme 4 "sponge bob imagination"
+ins_meme 5 "one cannot simply"
+
+ins_post 1 "first post" 1 1 "that feeling when..."
+ins_post 2 "tried to make meme generator" 2 1 "have no idea for memes"
+ins_post 3 "got a girlfriend" 2 1 "no idea what to do next"
+ins_post 4 "learning node" 3 2 "totally forgot how to c++"
+ins_post 5 "tryin to destroy dat ring" 5 3 "y cant we use gimlis axe"
