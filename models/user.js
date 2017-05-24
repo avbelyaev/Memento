017f8afd0ed2de44e6819bd715dd464e29fcb920
tokyonyquisd <antonsdl@gmail.com>
2017-05-25 01:44:46 +0300

    added user model stub methods

--- models/user.js
+++ models/user.js
@@ -13,13 +13,42 @@ var userSchema = Schema({
     type:       String
 });
 
-var user = mongoose.model('user', userSchema);
+var userModel = mongoose.model('user', userSchema);
 
-function create(login, name, password) {
-    
-    
+function create(_login, _name) {
+    userModel.create({
+        name: _name,
+        login: _login,
+        email: 'test@test.com',
+        type: 'regular'
+    }, function (err, usr) {
+        if (err) {
+            console.log('error while trying to create user with login "' + _login + '"');
+        } else {
+            console.log('entity with login "' + _login + '" was created');
+        }
+    });
 }
 
-function findAll() {
+function findByLogin(_login) {
+    userModel.find({login: _login}, function (err, usr) {
+        if (err) {
+            console.log('err while trying to get user by login"' + _login + '"');
+        } else {
+            return usr;
+        }
+    })
+}
 
+
+
+function findAll() {
+    userModel.find({}, function (err, users) {
+        if (err) {
+            console.log('err while trying to fetch all users');
+        } else {
+            console.log('there were fetched ' + users.length + ' entities');
+            return users;
+        }
+    })
 }
