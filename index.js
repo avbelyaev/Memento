d513efd54305f987d66232d6c6a5f61c989c4a17
tokyonyquisd <antonsdl@gmail.com>
2017-07-09 17:12:55 +0300

    added ExpressJS framework

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
 /**
  * Created by anthony on 07.05.17.
  */
+const express           = require('express');
 const server            = require("./server");
-const router            = require("./router");
-const requestHandlers   = require("./requestHandlers");
 const utils             = require("./utils");
 const db                = require('./db');
 const dbConfig          = require('./config/dbConfig');
 const serverConfig      = require('./config/serverConfig');
+const router              = require('./controllers/routes/router');
 
+var app = express();
 
-var handlers = [{
-        path: "/",
-        method: utils.METHOD_GET,
-        controller: requestHandlers.start
-    },
-    {
-        path: "/start",
-        method: utils.METHOD_GET,
-        controller: requestHandlers.start
-    },
-    {
-        path: "/upload",
-        method: utils.METHOD_POST,
-        controller: requestHandlers.upload
-    },
-    {
-        path: "/show",
-        method: utils.METHOD_GET,
-        controller: requestHandlers.show
-    },
-    {
-        path: "/showMeme",
-        method: utils.METHOD_GET,
-        controller: requestHandlers.showMeme
-    }
-];
+app.use('/', router);
 
 var dbInst = db.connect(dbConfig);
-server.start(router.route, handlers, serverConfig);
 
+server.start(app, serverConfig);
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 /**
  * Created by anthony on 07.05.17.
  */
-const express       = require('express');
 const http          = require("http");
-const url           = require("url");
 
 
-function start(route, handlers, config) {
-    var onRequest = function(rq, rsp) {
+function start(app, config) {
+    /*var onRequest = function(rq, rsp) {
         route(rq, rsp, handlers);
-    }
+    }*/
 
-    http.createServer(onRequest).listen(config.port);
-    console.log("server has started");
+    http.createServer(app).listen(config.port, function () {
+        console.log('server has started. port ' + config.port);
+    });
 
+    process.on('SIGINT', function () {
+        console.log('terminating server');
+    });
 }
 
 
