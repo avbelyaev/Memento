a081eaf3c20a06cf3752379e5a2de7e0664488d6
tokyonyquisd <antonsdl@gmail.com>
2017-07-22 23:44:44 +0300

    removed constant rels

--- controllers/routes/router.js
+++ controllers/routes/router.js
@@ -6,40 +6,36 @@ const router            = express.Router();
 const memeController    = require('../memeController');
 const userController    = require('../userController');
 const postController    = require('../postController');
-const rels              = require('../constants');
 
 router.get('/', function (rq, rsp) {
     console.log('root');
     rsp.send('root');
 });
 
-router.get(rels.MEME + ':id', memeController.findOneById);
-
+router.get('/meme/:id', memeController.findOneById);
 //https://stackoverflow.com/a/41521743/4504720
-router.post(rels.MEME + 'create', memeController.save);
+router.post('/meme/create', memeController.save);
+router.put('/meme/:id/update', memeController.update);
+router.delete('/meme/:id/delete', memeController.delete);
 
-router.put(rels.MEME + ':id/update', memeController.update);
-
-router.delete(rels.MEME + ':id/delete', memeController.delete);
-
-router.get(rels.MEMES, memeController.findAll);
+router.get('/memes/', memeController.findAll);
 
 //TODO JS Promises
 
 
-router.get(rels.USERS, function (rq, rsp) {
-
+router.get('/users', function (rq, rsp) {
+    console.log("users::get")
 });
 
 
 
 
 
-router.get(rels.POSTS, function (rq, rsp) {
+router.get('/posts', function (rq, rsp) {
 
 });
 
-router.get(rels.USERS + ':id', function (rq, rsp) {
+router.get('/user/:id', function (rq, rsp) {
     var id = rq.params.id;
     console.log('user id:' + id);
     rsp.send('id: ' + id);
